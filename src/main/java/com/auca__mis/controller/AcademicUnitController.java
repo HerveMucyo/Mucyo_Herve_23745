a377961e60ce4cf3690a21c1437b3db5e55e65a8
BugingoBlaise <blaisemugisha1@gmail.com>
2023-11-16 01:03:06 +0200

    academic unit add done

--- src/main/java/com/auca__mis/controller/AcademicUnitController.java
+++ src/main/java/com/auca__mis/controller/AcademicUnitController.java
@@ -21,39 +21,23 @@ public class AcademicUnitController {
         this.IAcademicUnitService = IAcademicUnitService;
     }
 
-
     @GetMapping("/acadUnit")
     public String showAcademicUnitDashboard(Model model) {
-
         List<AcademicUnit> academicUnitList = IAcademicUnitService.findAll();
         model.addAttribute("acadUnit",new AcademicUnit());
         model.addAttribute("academicUnitList", academicUnitList);
         return "academicUnit";
-
     }
     @PostMapping("/acadUnit/create")
     public String createAcademicUnit(@ModelAttribute("acadUnit") AcademicUnit academicUnit) {
 
-        randomCodes(academicUnit);
+
         IAcademicUnitService.saveAcademicUnit(academicUnit);
         return "redirect:/acadUnit";
     }
 
-    public static void randomCodes(@ModelAttribute("acadUnit") AcademicUnit academicUnit) {
-        int min = 10000; // Smallest 5-digit number
-        int max = 99999; // Largest 5-digit number
 
-        Random random = new Random();
-        int randomCode = random.nextInt(max - min + 1) + min;
-        academicUnit.setCode(String.valueOf(randomCode));
-        academicUnit.setUnit(EAcademicUnit.PROGRAMME);
-    }
 
-    //    @RequestMapping(value = "semester/update", method = RequestMethod.GET)
-//    public String updateSemester(@ModelAttribute("semester") Semester semester) {
-//        semesterService.updateSemester(semester);
-//        return "redirect:/semester";
-//    }
     @RequestMapping(value = "/acadUnit/delete/{id}", method = RequestMethod.GET)
     public String deleteAcademicUnit(@PathVariable AcademicUnit academicUnit) {
         IAcademicUnitService.deleteUnit(academicUnit);
--- src/main/java/com/auca__mis/service/implementation/IAcademicUnitServiceImplementation.java
+++ src/main/java/com/auca__mis/service/implementation/IAcademicUnitServiceImplementation.java
@@ -2,14 +2,17 @@ package com.auca__mis.service.implementation;
 
 import com.auca__mis.controller.AcademicUnitController;
 import com.auca__mis.dao.IAcademicUnitDao;
+import com.auca__mis.enums.EAcademicUnit;
 import com.auca__mis.model.AcademicUnit;
 import com.auca__mis.service.IAcademicUnitService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 @Transactional
@@ -23,7 +26,7 @@ public class IAcademicUnitServiceImplementation implements IAcademicUnitService 
     @Override
     public AcademicUnit saveAcademicUnit(AcademicUnit academicUnit) {
 
-        AcademicUnitController.randomCodes(academicUnit);
+        randomCodes(academicUnit);
 
         return academicUnitDao.save(academicUnit);
     }
@@ -50,5 +53,17 @@ public class IAcademicUnitServiceImplementation implements IAcademicUnitService 
     public AcademicUnit findUnitById(AcademicUnit academicUnit) {
         return this.academicUnitDao.findById(academicUnit.getId())
                 .orElseThrow(() -> new RuntimeException("Unit not found with code: " + academicUnit.getCode()));
+    }
+    public static void randomCodes(AcademicUnit academicUnit) {
+        int min = 50000; // Smallest 5-digit number
+        int max = 100000; // Largest 5-digit number
+
+        Random random = new Random();
+        int randomCode = random.nextInt(max - min + 1) + min;
+
+        academicUnit.setCode(String.valueOf(randomCode));
+        academicUnit.setUnit(EAcademicUnit.PROGRAMME);
+        academicUnit.setAcademicUnit(academicUnit);
+        academicUnit.setName(academicUnit.getName());
     }
 }
